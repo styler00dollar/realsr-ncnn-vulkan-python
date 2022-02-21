acce251b752e4ac8696f5b3743f7dfef32acc8f5
Joey Ballentine <joeyjballentine@gmail.com>
2022-02-21 17:45:19 -0500

    Add cleanup function support

--- realsr_ncnn_vulkan_python/realsr_wrapped.cpp
+++ realsr_ncnn_vulkan_python/realsr_wrapped.cpp
@@ -5,7 +5,8 @@
 
 RealSRWrapped::RealSRWrapped(int gpuid, bool tta_mode) : RealSR(gpuid, tta_mode) {}
 
-int RealSRWrapped::load(const StringType &parampath, const StringType &modelpath) {
+int RealSRWrapped::load(const StringType &parampath, const StringType &modelpath)
+{
 #if _WIN32
     return RealSR::load(*parampath.wstr, *modelpath.wstr);
 #else
@@ -13,17 +14,25 @@ int RealSRWrapped::load(const StringType &parampath, const StringType &modelpath
 #endif
 }
 
-int RealSRWrapped::process(const Image &inimage, Image outimage) {
+int RealSRWrapped::process(const Image &inimage, Image outimage)
+{
     int c = inimage.elempack;
-    ncnn::Mat inimagemat = ncnn::Mat(inimage.w, inimage.h, (void*) inimage.data, (size_t) c, c);
-    ncnn::Mat outimagemat = ncnn::Mat(outimage.w, outimage.h, (void*) outimage.data, (size_t) c, c);
+    ncnn::Mat inimagemat = ncnn::Mat(inimage.w, inimage.h, (void *)inimage.data, (size_t)c, c);
+    ncnn::Mat outimagemat = ncnn::Mat(outimage.w, outimage.h, (void *)outimage.data, (size_t)c, c);
     return RealSR::process(inimagemat, outimagemat);
 };
 
-uint32_t get_heap_budget(int gpuid) {
+int RealSRWrapped::cleanup()
+{
+    return RealSR::cleanup();
+};
+
+uint32_t get_heap_budget(int gpuid)
+{
     return ncnn::get_gpu_device(gpuid)->get_heap_budget();
 }
 
-int get_gpu_count() {
+int get_gpu_count()
+{
     return ncnn::get_gpu_count();
 }
